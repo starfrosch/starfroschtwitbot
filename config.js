489987eb8ab52a1d8594d1cc2de3ca322a0e1b6b
Markus Koller <markus.koller@starfrosch.ch>
2018-01-09 20:01:54 +0100

    config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+
+module.exports = {
+  consumer_key: 'kUepUdh1sFzmJN527moFDaf3P',  
+  consumer_secret: 'gDCXJGDYEyQjPl6RpcnLRXCKPrhz2XfMRcs6JjcOKaGP0qnbJx',
+  access_token: '949042985743847425-7fS4SOfU8bDrufd1kQW4eEQsjGSjcWE',  
+  access_token_secret: 'WrqN2DXo9eDyuv5p3KcrijeCHENrMzSdI8H3VLsMsJMdQ'
+}
