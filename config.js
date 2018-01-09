cc42dd038d750bb8b8fc3f44bf141595adb9654a
Markus Koller <markus.koller@starfrosch.ch>
2018-01-09 20:30:39 +0100

    Heroku ENV Vars

---
+++ config.js
@@ -0,0 +1,14 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+
+module.exports = {
+  consumer_key: process.env.twitter_consumer_key,
+  consumer_secret: process.env.twitter_consumer_secret,
+  access_token: process.env.twitter_access_token,
+  access_token_secret: process.env.twitter_access_token_secret
+}
