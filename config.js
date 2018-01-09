74fb5e91641b5b30852ca472ebc8485eba904ee5
Markus Koller <markus.koller@starfrosch.ch>
2018-01-09 20:25:37 +0100

    Heroku env vars

--- config.js
+++ config.js
@@ -7,8 +7,8 @@
  */
 
 module.exports = {
-  consumer_key: 'kUepUdh1sFzmJN527moFDaf3P',  
-  consumer_secret: 'gDCXJGDYEyQjPl6RpcnLRXCKPrhz2XfMRcs6JjcOKaGP0qnbJx',
-  access_token: '949042985743847425-7fS4SOfU8bDrufd1kQW4eEQsjGSjcWE',  
-  access_token_secret: 'WrqN2DXo9eDyuv5p3KcrijeCHENrMzSdI8H3VLsMsJMdQ'
+  consumer_key: process.env.twitter_consumer_key,
+  consumer_secret: process.env.twitter_consumer_secret,
+  access_token: process.env.twitter_access_token,
+  access_token_secret: process.env.twitter_access_token_secret
 }
