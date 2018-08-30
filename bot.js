ccfd992dbd4a596387e05054cf94f516dd9cb301
Markus Koller <markus.koller@starfrosch.ch>
2018-08-30 21:00:05 +0200

    Wait 10 Seconds

--- bot.js
+++ bot.js
@@ -358,16 +358,20 @@ function followFriends () {
             }
           });
       // wait 10 seconds
-      await sleep(10000);
+      sleep(10000);
       });
     }
   });
 };
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
 }
-
 
 function randIndex (arr) {
   var index = Math.floor(arr.length*Math.random());
